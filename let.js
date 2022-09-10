a56e9213278cc714c4f95c1fb37b00c8f96488d2
Christopher langley <christopherlangley@Christophers-MacBook-Pro.local>
2022-09-09 19:35:10 -0700

    function-drills

---
+++ let.js
@@ -0,0 +1,7 @@
+/*
+  Write a function that is called theEliminator, which takes in two arguments, contestants (which will each be an array of strings), and loser (which will be a string).
+  The function should loop over the array of contestant names. If the loser string appears in the array, splice it out. Return the new contestants array.
+*/
+function
+  ////////////////// PROBLEM 17 ////////////////////
+  let() { }
